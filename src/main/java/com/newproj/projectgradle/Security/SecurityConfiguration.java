4300e907813671cfedcb26c69f976fbb453cd77c
neeteshtmrkr <tamrakarnitesh21@gmail.com>
2021-06-21 13:40:11 +0545

    In Memory Auth done with the User Roles addded

--- src/main/java/com/newproj/projectgradle/Security/SecurityConfiguration.java
+++ src/main/java/com/newproj/projectgradle/Security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.newproj.projectgradle.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,11 +16,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("neetesh")
                 .password("nepal123")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("q!w@erty")
+                .roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
